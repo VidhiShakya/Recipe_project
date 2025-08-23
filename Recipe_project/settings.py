f8770a6e2227307ddc127a8fb70936399d5bb7ae
Vidhi Shakya <vidhishakya06@gmail.com>
2025-08-23 21:46:01 +0530

    .

--- Recipe_project/settings.py
+++ Recipe_project/settings.py
@@ -79,9 +79,19 @@ WSGI_APPLICATION = 'Recipe_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL', default='sqlite:///db.sqlite3'))
-}
+DATABASE_URL = config('DATABASE_URL', default='')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
